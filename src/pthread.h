7db3895441b5064a1c945c2ba34fc4a82ee7e496
Luba Tang <luba@skymizer.com>
2014-07-25 09:05:00 +0800

    Add comments of pthread.

--- src/pthread.h
+++ src/pthread.h
@@ -1,5 +1,17 @@
+/*===- pthread.h ----------------------------------------------------------===//
+//
+//                          The LibC-Bench Project
+//
+//  This file is distributed under the GPLv2 License.
+//  See LICENSE for details.
+//
+//===----------------------------------------------------------------------===*/
 #ifndef LIBCBENCH_PTHREAD_H
 #define LIBCBENCH_PTHREAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void *emptyfunc(void *dummy);
 
@@ -12,4 +24,9 @@ size_t b_pthread_create_serial1(void *dummy);
 void *lockunlock(void *mut);
 
 size_t b_pthread_uselesslock(void *dummy);
+
+#ifdef __cplusplus
+} // end of extern
+#endif
+
 #endif
