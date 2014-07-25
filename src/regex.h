64d128a211e76e6b40ef1f8c257c4941ebe9c6a1
Luba Tang <luba@skymizer.com>
2014-07-25 13:31:43 +0800

    Add comments.

--- src/regex.h
+++ src/regex.h
@@ -1,7 +1,24 @@
+/*===- regex_test.h -------------------------------------------------------===//
+//
+//                          The LibC-Bench Project
+//
+//  This file is distributed under the GPLv2 License.
+//  See LICENSE for details.
+//
+//===----------------------------------------------------------------------===*/
 #ifndef LIBCBENCH_REGEX_H
 #define LIBCBENCH_REGEX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 size_t b_regex_compile(char *s);
 
 size_t b_regex_search(char *s);
+
+#ifdef __cplusplus
+} // end of extern
+#endif
+
 #endif
